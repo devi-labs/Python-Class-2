ad2c0f9c85ec3face41b5d9311a1fcb93af3050a
Devina Dhawan <79779232+devi-labs@users.noreply.github.com>
2022-05-29 15:44:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+x = "Happy Mothers Day! " 
+y = "Sonia Dhawan" 
+z = "Devina" 
+b = "Gina"
+age = "87"
+age2 = "20"
+
+#str()
+print ((int(age)+int(age2))/2)
+print (x+z)
+print (x+y)
+
+
+               My name is Devina
+           This was written on 05/08/2022
+"""
+name = input("Hey GURLLLL whats your name?: ")
+print ("OOOO! HEY GURL NAMED: "+ name)
+number = input("Whats your number?!")
+print ("imaaaaa call you at: " + number)
+
+
+
+
+
+
+
+
+
